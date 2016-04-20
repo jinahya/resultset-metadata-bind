bd17a4ae7daede32ec0a3fcceb1011e1e49a02e4
Jin Kwon <onacit@gmail.com>
2016-04-20 19:15:50 +0900

    updated

--- src/main/java/com/github/jinahya/sql/resultset/metadata/bind/ResultSetColumn.java
+++ src/main/java/com/github/jinahya/sql/resultset/metadata/bind/ResultSetColumn.java
@@ -19,7 +19,6 @@ package com.github.jinahya.sql.resultset.metadata.bind;
  *
  * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
  */
-//@lombok.Data
 public class ResultSetColumn {
 
     // ------------------------------------------------------------- catalogName
@@ -171,8 +170,17 @@ public class ResultSetColumn {
         return scale;
     }
 
-    public void setScale(int scale) {
+    public void setScale(final int scale) {
         this.scale = scale;
+    }
+
+    public int scale() {
+        return getScale();
+    }
+
+    public ResultSetColumn scale(final int scale) {
+        setScale(scale);
+        return this;
     }
 
     // -------------------------------------------------------------- schemaName
@@ -184,87 +192,197 @@ public class ResultSetColumn {
         this.schemaName = schemaName;
     }
 
+    public String schemaName() {
+        return getSchemaName();
+    }
+
+    public ResultSetColumn schemaName(final String schemaName) {
+        setSchemaName(schemaName);
+        return this;
+    }
+
     // --------------------------------------------------------------- tableName
     public String getTableName() {
         return tableName;
     }
 
-    public void setTableName(String tableName) {
+    public void setTableName(final String tableName) {
         this.tableName = tableName;
     }
 
+    public String tableName() {
+        return getTableName();
+    }
+
+    public ResultSetColumn tableName(final String tableName) {
+        setTableName(tableName);
+        return this;
+    }
+
+    // ----------------------------------------------------------- autoIncrement
     public boolean isAutoIncrement() {
         return autoIncrement;
     }
 
-    public void setAutoIncrement(boolean autoIncrement) {
+    public void setAutoIncrement(final boolean autoIncrement) {
         this.autoIncrement = autoIncrement;
     }
 
+    public boolean autoIncrement() {
+        return isAutoIncrement();
+    }
+
+    public ResultSetColumn autoIncrement(final boolean autoIncrement) {
+        setAutoIncrement(autoIncrement);
+        return this;
+    }
+
+    // ----------------------------------------------------------- caseSensitive
     public boolean isCaseSensitive() {
         return caseSensitive;
     }
 
-    public void setCaseSensitive(boolean caseSensitive) {
+    public void setCaseSensitive(final boolean caseSensitive) {
         this.caseSensitive = caseSensitive;
     }
 
+    public boolean caseSensitive() {
+        return isCaseSensitive();
+    }
+
+    public ResultSetColumn caseSensitive(final boolean caseSensitive) {
+        setCaseSensitive(caseSensitive);
+        return this;
+    }
+
+    // ---------------------------------------------------------------- currency
     public boolean isCurrency() {
         return currency;
     }
 
-    public void setCurrency(boolean currency) {
+    public void setCurrency(final boolean currency) {
         this.currency = currency;
     }
 
+    public boolean currency() {
+        return isCurrency();
+    }
+
+    public ResultSetColumn currency(final boolean currency) {
+        setCurrency(currency);
+        return this;
+    }
+
+    // ------------------------------------------------------ definitelyWritable
     public boolean isDefinitelyWritable() {
         return definitelyWritable;
     }
 
-    public void setDefinitelyWritable(boolean definitelyWritable) {
+    public void setDefinitelyWritable(final boolean definitelyWritable) {
         this.definitelyWritable = definitelyWritable;
     }
 
+    public boolean definitelyWritable() {
+        return isDefinitelyWritable();
+    }
+
+    public ResultSetColumn definitelyWritable(
+            final boolean definitelyWritable) {
+        setDefinitelyWritable(definitelyWritable);
+        return this;
+    }
+
+    // ---------------------------------------------------------------- nullable
     public int getNullable() {
         return nullable;
     }
 
-    public void setNullable(int nullable) {
+    public void setNullable(final int nullable) {
         this.nullable = nullable;
     }
 
+    public int nullable() {
+        return getNullable();
+    }
+
+    public ResultSetColumn nullable(final int nullable) {
+        setNullable(nullable);
+        return this;
+    }
+
+    // ---------------------------------------------------------------- readOnly
     public boolean isReadOnly() {
         return readOnly;
     }
 
-    public void setReadOnly(boolean readOnly) {
+    public void setReadOnly(final boolean readOnly) {
         this.readOnly = readOnly;
     }
 
+    public boolean readOnly() {
+        return isReadOnly();
+    }
+
+    public ResultSetColumn readOnly(final boolean readOnly) {
+        setReadOnly(readOnly);
+        return this;
+    }
+
+    // -------------------------------------------------------------- searchable
     public boolean isSearchable() {
         return searchable;
     }
 
-    public void setSearchable(boolean searchable) {
+    public void setSearchable(final boolean searchable) {
         this.searchable = searchable;
     }
 
+    public boolean searchable() {
+        return isSearchable();
+    }
+
+    public ResultSetColumn searchable(final boolean searchable) {
+        setSearchable(searchable);
+        return this;
+    }
+
+    // ------------------------------------------------------------------ signed
     public boolean isSigned() {
         return signed;
     }
 
-    public void setSigned(boolean signed) {
+    public void setSigned(final boolean signed) {
         this.signed = signed;
     }
 
+    public boolean signed() {
+        return isSigned();
+    }
+
+    public ResultSetColumn signed(final boolean signed) {
+        setSigned(signed);
+        return this;
+    }
+
+    // ---------------------------------------------------------------- writable
     public boolean isWritable() {
         return writable;
     }
 
-    public void setWritable(boolean writable) {
+    public void setWritable(final boolean writable) {
         this.writable = writable;
     }
 
+    public boolean writable() {
+        return isWritable();
+    }
+
+    public ResultSetColumn writable(final boolean writable) {
+        setWritable(writable);
+        return this;
+    }
+
+    // -------------------------------------------------------------------------
     private String catalogName;
 
     private String columnClassName;
